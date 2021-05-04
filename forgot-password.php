4ddd74ecaf0d36e71685c38e13db868244c385ad
anjgesim <angela.gesim@cvsu.edu.ph>
2021-05-04 15:02:50 +0800

    lab activities

--- forgot-password.php
+++ forgot-password.php
@@ -1,81 +1,60 @@
 <?php
-$conn = mysqli_connect('localhost','root',"",'accounts');
-if(isset($_POST['submit'])){
-	$email = mysqli_real_escape_string($conn, $_POST['email']);
-	$query = "select * from members where email ='$email'";
-	$run = mysqli_query($conn, $query);
-	if(mysqli_num_rows($run)>0){
-		$row = mysqli_fetch_array($run);
-		$db_email = $row['email'];
-		$db_id = $row['id'];
-		$token =uniqid(md5(time()));
-		$query ="INSERT INTO password_reset(id, email, token) VALUES(NULL,'$email','$token')";
-		if(mysqli_query($conn, $query)){
-			$to = $db_email;
-			$subject = "Password reset link";
-			$message = "Click <a href='https://YOUR_wEBSITE.com/reset.php?token=$token'>here</a> to reset your password.";
-			
-
-			mail($to, $subject, $message, $headers);
-			$msg = "<div class='alert alert-success'>Password reset link has been sent to the email.</div>";
-		}
-	}else{
-		$msg ="<div class='alert alert danger'> User not found</div>";
-
-	}
-}
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="styles.css">
-
-	<title>Forgot Password</title>
-
-<head>
-	
-	<body>
-		<div class="container">
-			<div class="row">
-			<div class="">
-				<h2>Forgot Password</h2><hr>
-				<form action="method="post">
-					<div class="form-group">
-						<label for="">Enter Email</label>
-						<input type="email" name="email" class="form-control">
-					</div>
-
-					<div class="form-group">
-						<button name="submit" class="btn btn-primary btn-block"> Submit</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-
-
-
-	</body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<title> Forgot Password Recovery (Reset)</title>
+<link rel='stylesheet' href='css/style.css' type='text/css' media='all' />
 </head>
+<body>
+<div style="width:700px; margin:50 auto;">
+
+<h2>Forgot Password Recovery</h2>   
+
+<?php
+include('db_conn.php');
+if(isset($_POST["email"]) && (!empty($_POST["email"]))){
+$email = $_POST["email"];
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+$email = filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email) {
+  	$error ="<p>Invalid email address please type a valid email address!</p>";
+	}else{
+	$sel_query = "SELECT * FROM `password_reset_temp` WHERE email='".$email."'";
+	$results = mysqli_query($conn,$sel_query);
+	$row = mysqli_num_rows($results);
+	if ($row==""){
+		$error .= "<p>No user is registered with this email address!</p>";
+
+		}else{
+		}
+	$expFormat = mktime(date("H"), date("i"), date("s"), date("m")  , date("d")+1, date("Y"));
+	$expDate = date("Y-m-d H:i:s",$expFormat);
+	$key = md5(2418*2);
+	$addKey = substr(md5(uniqid(rand(),1)),3,10);
+	$key = $key . $addKey;
+// Insert Temp Table
+mysqli_query($conn,
+"INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
+VALUES ('".$email."', '".$key."', '".$expDate."');");
+	}
+
+		}	
+?>
+<form method="post" action="reset-password.php" name="reset"><br /><br />
+<label><strong>Enter Your Email Address:</strong></label><br /><br />
+<input type="email" name="email" placeholder="username@email.com" />
+<br /><br />
+<input type="submit" value="Reset Password"/>
+</form>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+
+<br /><br />
+
+</div>
+</body>
+</html>
